fb765cc874b263d3daf1ad32532da29be22e6283
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2018-01-04 13:13:38 +0000

    simulated backend database frontend now updates when listing is posted

--- app/makersbnb.rb
+++ app/makersbnb.rb
@@ -12,7 +12,12 @@ class Makersbnb < Sinatra::Base
     content_type :json
     listing = Listing.new('Leafy Towers', 'Tall and Leafy', 2)
     listing2 = Listing.new('Rocky Towers', 'Tall and Rocky', 2)
-    entries = [listing, listing2]
+    @listing3 = Listing.show_listing
+    if !!@listing3
+      entries = [listing, listing2, @listing3]
+    else
+      entries = [listing, listing2]
+    end
     entries.map{ |entry| { name: entry.name, bio: entry.bio, guests: entry.guests }}.to_json
   end
 
