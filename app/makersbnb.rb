992414781302a8a31b07b3da4908031bdd127d16
Lan Pham <lanpham312@gmail.com>
2018-01-04 11:50:01 +0000

    created get page which sends listings to frontend

--- app/makersbnb.rb
+++ app/makersbnb.rb
@@ -4,16 +4,22 @@ require 'sinatra/base'
 require './lib/listing.rb'
 require 'json'
 
-
-
 class Makersbnb < Sinatra::Base
+
+
+  get '/listings' do
+    headers 'Access-Control-Allow-Origin' => '*'
+    content_type :json
+    listing = Listing.new('Leafy Towers', 'Tall and Leafy', 2)
+    listing2 = Listing.new('Rocky Towers', 'Tall and Rocky', 2)
+    entries = [listing, listing2]
+    entries.map{ |entry| { name: entry.name, bio: entry.bio, guests: entry.guests }}.to_json
+  end
 
   post '/listings' do
     headers 'Access-Control-Allow-Origin' => '*'
     data = JSON.parse(request.body.read)
     Listing.create_listing(data['name'], data['bio'], data['guests'])
-    @listing = Listing.show_listing
-    p @listing
   end
 
   run! if app_file == $0
--- lib/listing.rb
+++ lib/listing.rb
@@ -1,13 +1,13 @@
 class Listing
-  attr_reader :name, :bio, :guest
-  def initialize(name, bio, guest)
+  attr_reader :name, :bio, :guests
+  def initialize(name, bio, guests)
     @name = name
     @bio = bio
-    @guest = guest
+    @guests = guests
   end
 
-  def self.create_listing(name, bio, guest)
-    @listing = Listing.new(name, bio, guest)
+  def self.create_listing(name, bio, guests)
+    @listing = Listing.new(name, bio, guests)
   end
 
   def self.show_listing
